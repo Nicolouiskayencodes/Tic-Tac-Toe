7f279d7399d3bffc11cb2bad0dab3864dc2c5e2a
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2024-07-10 20:59:53 -0700

    Create new game button and name inputs

--- script.js
+++ script.js
@@ -32,21 +32,14 @@ function Gameboard() {
   return {getBoard, markCell, printBoard}
 }
 
-// function Cell() {
-//   let value = 0;
-//   const addMark = (player) => {
-//     value = player;
-//   };
-//   const getValue = () => value;
-//   return {addMark, getValue};
-// }
 
 function GameController(
-  playerOneName = "Player One",
-  playerTwoName = "Player Two"
+  playerOneName,
+  playerTwoName
 ) {
   const board = Gameboard();
-
+  let gameOver = 0;
+  const getGameover = () => gameOver;
   const players = [
     {
       name: playerOneName,
@@ -78,15 +71,82 @@ function GameController(
     if ((board.getBoard()[row][0].getValue() === board.getBoard()[row][1].getValue() && board.getBoard()[row][0].getValue() === board.getBoard()[row][2].getValue()) || (board.getBoard()[0][column].getValue() === board.getBoard()[1][column].getValue() && board.getBoard()[0][column].getValue() === board.getBoard()[2][column].getValue()) || (board.getBoard()[1][1].getValue() !== 0 && board.getBoard()[1][1].getValue() === board.getBoard()[0][0].getValue() && board.getBoard()[1][1].getValue() === board.getBoard()[2][2].getValue()) || (board.getBoard()[1][1].getValue() !== 0 && board.getBoard()[1][1].getValue() === board.getBoard()[0][2].getValue() && board.getBoard()[1][1].getValue() === board.getBoard()[2][0].getValue())) {
       board.printBoard();
       console.log(getActivePlayer().name + " wins!");
-      game = GameController();
+      gameOver=1;
       return;
+    } else if (board.getBoard()[0][0].getValue() !== 0 && board.getBoard()[0][1].getValue() !== 0 && board.getBoard()[0][2].getValue() !== 0 && board.getBoard()[1][0].getValue() !== 0 && board.getBoard()[1][1].getValue() !== 0 && board.getBoard()[1][2].getValue() !== 0 && board.getBoard()[2][0].getValue() !== 0 && board.getBoard()[2][1].getValue() !== 0 && board.getBoard()[2][2].getValue() !== 0) {
+      gameOver=2;
     }
 
     switchPlayerTurn();
     printNewRound();
   };
   printNewRound();
-  return {playRound, getActivePlayer};
+  return {playRound, getActivePlayer, getGameover, getBoard: board.getBoard};
 }
 
-let game = GameController();
+function Display(nameOne, nameTwo) {
+  let game = GameController(nameOne, nameTwo);
+  const turnDiv = document.querySelector("#playerTurn");
+  const boardDiv = document.querySelector("#gameboard");
+  let gameOver = 0;
+  
+  const updateDisplay = () => {
+    if (gameOver === 1) {
+      return;
+    }
+    boardDiv.textContent = "";
+    board = game.getBoard();
+    turnDiv.textContent = game.getActivePlayer().name + "'s turn.";
+    let i=0;
+    board.forEach(row => {
+      let j = i++;
+      row.forEach((cell, index) => {
+        const cellButton = document.createElement("button");
+          cellButton.classList.add("cell");
+          cellButton.dataset.row = j;
+          cellButton.dataset.column = index;
+          if (cell.getValue() !== 0){
+          cellButton.textContent = cell.getValue();
+          }
+          boardDiv.appendChild(cellButton);
+      })
+    })
+    }
+    function clickHandlerBoard(e) {
+      if (gameOver === 1) {
+        return;
+      }
+      const selectedRow = e.target.dataset.row;
+      const selectedColumn = e.target.dataset.column;
+      game.playRound(selectedRow, selectedColumn);
+      updateDisplay();
+      if(game.getGameover() === 1) {
+        turnDiv.textContent = game.getActivePlayer().name + " wins!";
+        gameOver = 1;
+      } else if (game.getGameover() === 2) {
+        turnDiv.textContent = "It's a tie!";
+        gameOver = 1;
+      }
+  }
+  boardDiv.addEventListener("click", clickHandlerBoard);
+  updateDisplay()
+}
+
+function Form() {
+  let nameOne;
+  let nameTwo;
+  if (document.querySelector("#NameOne").value === "") {
+    nameOne = "Player One"
+  } else{
+    nameOne = document.querySelector("#NameOne").value;
+  }
+  document.querySelector("#NameOne").textContent = "";
+  if (document.querySelector("#NameTwo").value === "") {
+    nameTwo = "Player Two"
+  } else{
+    nameTwo = document.querySelector("#NameTwo").value;
+  }
+  document.querySelector("#NameTwo").textContent = "";
+  Display(nameOne, nameTwo);
+}
+document.querySelector("#newGame").addEventListener("click", () => {Form()});
